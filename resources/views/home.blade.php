d88d27edf833853ac0378483f25bbc41ee330a70
Christopher Howie <christopher.howie@egov.com>
2019-02-05 21:12:15 -0600

    Adding resume details

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,25 +4,24 @@
 
     <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
         <div class="w-100">
-            <h1 class="mb-0">Clarence
-                <span class="text-primary">Taylor</span>
+            <h1 class="mb-0">Christopher
+                <span class="text-primary">Howie</span>
             </h1>
-            <div class="subheading mb-5">3542 Berry Street · Cheyenne Wells, CO 80810 · (317) 585-8468 ·
-                <a href="mailto:name@email.com">name@email.com</a>
+            <div class="subheading mb-5">4432 Old Pond Trl · Dimondale, MI 48821 · (260) 615-4382 ·
+                <a href="mailto:me@christopherhowie.com">me@christopherhowie.com</a>
             </div>
-            <p class="lead mb-5">I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
+            <p class="lead mb-5">
+            Web Developer with 20 years of programming experience in a variety of environments and 6 years of web application development experience using PHP MVC frameworks.
+            </p>
             <div class="social-icons">
-                <a href="#">
+                <a href="https://www.linkedin.com/in/christopherhowie/">
                     <i class="fab fa-linkedin-in"></i>
                 </a>
-                <a href="#">
+                <a href="https://github.com/chowie">
                     <i class="fab fa-github"></i>
                 </a>
-                <a href="#">
+                <a href="https://twitter.com/ChrisHowie">
                     <i class="fab fa-twitter"></i>
-                </a>
-                <a href="#">
-                    <i class="fab fa-facebook-f"></i>
                 </a>
             </div>
         </div>
