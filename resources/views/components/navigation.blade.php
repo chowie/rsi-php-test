715c9fdccce7f8d1e108998d77f9723ee66697e5
Christopher Howie <christopher.howie@egov.com>
2019-02-07 14:30:00 -0600

    Cleaning up the landing page

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -13,7 +13,7 @@
             <li class="nav-item">
                 <a class="nav-link" href="/contact"><i class="fas fa-at"></i> Contact</a>
             </li>
-            @foreach(['about','experience','education','skills','interests','awards'] as $navItem)
+            @foreach(['about','skills'] as $navItem)
                 @component('components.nav-item')
                     {{$navItem}}
                 @endcomponent
