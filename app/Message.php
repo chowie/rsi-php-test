d3536ed74e2489bbfed54da5ca28f9dda4390921
Christopher Howie <christopher.howie@egov.com>
2019-02-07 16:24:17 -0600

    setting up email

--- app/Message.php
+++ app/Message.php
@@ -2,14 +2,31 @@
 
 namespace App;
 
+use App\Events\MessageCreated;
+use App\Listeners\SendContactMessage;
+use App\Mail\SendContactMessageEmail;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Message extends Model
 {
+
+    use Notifiable;
 
     protected $fillable = [
         'name',
         'email',
         'message'
     ];
+
+    protected $dispatchesEvents = [
+        'created' => MessageCreated::class,
+    ];
+
+    public static function created($message)
+    {
+        Mail::to($request->user())->send(new SendContactMessageEmail($message));
+    }
+
+
 }
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Resume - Start Bootstrap Theme</title>
+
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="{{url('/')}}{{mix('css/app.css')}}">
+    <link rel="stylesheet" href="{{url('/')}}{{mix('css/theme.css')}}">
+
+    <script defer src="https://use.fontawesome.com/releases/v5.7.1/js/all.js" integrity="sha384-eVEQC9zshBn0rFj4+TU78eNA19HMNigMviK/PU/FFjLXqa/GKPgX58rvt5Z8PLs7" crossorigin="anonymous"></script>
+
+</head>
+
+<body id="page-top">
+
